4d42d82b4f20f48b3303ab69a10ec2a175cdd69c
Felipe Oliveira <felipecvo@gmail.com>
2024-09-20 08:47:00 -0300

    feat: localize app

--- app/src/main/java/br/com/fiap/mad/cinema/MainActivity.kt
+++ app/src/main/java/br/com/fiap/mad/cinema/MainActivity.kt
@@ -1,12 +1,21 @@
 package br.com.fiap.mad.cinema
 
+import android.app.LocaleManager
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private val localeList = listOf("pt-BR", "en-US", "en-GB", "es", "pt", "en")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,37 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>, view: View?, position: Int, id: Long) {
+                println("value: $id")
+                val selectedLocale = localeList[position]
+                val locale = Locale.forLanguageTag(selectedLocale)
+
+                changeLocale(locale)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
+    }
+
+    private fun changeLocale(locale: Locale) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Use LocaleManager for Android 12 and later
+            val localeManager = application.getSystemService(LocaleManager::class.java)
+            localeManager.applicationLocales = LocaleList(locale)
+        } else {
+            // For earlier versions, update the app's configuration manually
+            Locale.setDefault(locale)
+
+            val resources = application.resources
+            val configuration = resources.configuration
+            configuration.setLocale(locale)
+            resources.updateConfiguration(configuration, resources.displayMetrics)
         }
     }
 }
